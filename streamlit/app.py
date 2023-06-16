2e0652e19e226350cf3b471b84cd36372fea757d
MuhammedAjmalG <119664819+MuhammedAjmalG@users.noreply.github.com>
2023-06-16 12:18:39 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from  helper import query_point_creator
 import helper
 import pickle
 import xgboost as XGBClassifier
@@ -17,7 +18,7 @@ q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
 if st.button('Find'):
-    query = helper.query_point_creator(q1,q2)
+    query = query_point_creator(q1,q2)
     result = model.predict(query)[0]
 
     if result:
